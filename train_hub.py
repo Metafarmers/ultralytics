1915c0f51869199da150dbab3fdd1134bfe28d32
wonjoonyun1 <141394208+wonjoonyun1@users.noreply.github.com>
2024-06-11 16:36:22 +0900

    Delete train_hub.py

--- train_hub.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO, checks, hub
-checks()
-
-hub.login('18b2d937cf0ed370aceba118601b1bc30e7fd219e9')
-
-model = YOLO('https://hub.ultralytics.com/models/GyXxzl7k8f4BG437MtKb')
-
-results = model.train()
